2a5b6ed66fd8e3489cc716364bcedbd61c8e2036
HuuThuy3004 <144524343+HuuThuy3004@users.noreply.github.com>
2023-12-27 15:45:02 +0700

    Add files via upload

---
+++ session12/baitap/baitap.js
@@ -0,0 +1,44 @@
+let todoList = ["Go to school at 7am" , "Read book at 12am"];
+let work;
+let exit = true;
+
+while (exit == true) {
+    let input = prompt("Nhap tung chu cai sau (C-R-U-D)");
+    switch (input) {
+        case "C":
+            work = prompt("Them cong viec ban muon:");
+            todoList.push(work);
+            console.log(todoList);
+            break;
+        case "R":
+            console.log("Danh sach cac cong viec");
+            for (let works of todoList) {
+                console.log(works);
+            }
+            break; 
+        case "U":
+            var index , content;
+            index = prompt("Chon vi tri cong viec ban muon update");
+            content = prompt("Noi dung update la gi?");
+            todoList.splice(index-1 , 1 , content);
+            console.log(todoList);          
+            break;
+        case "D":
+            index = prompt("Chon vi tri cong viec ban muon delete");
+            todoList.splice(index-1 , 1);
+            console.log(todoList);
+            break;             
+        default:
+            exit = false;
+            break;
+    }
+}
+
+
+    
+
+
+
+
+
+
